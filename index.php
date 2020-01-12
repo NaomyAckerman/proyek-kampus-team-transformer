119e9abad6c861e44a32e8005ff04482314d6695
VennaErsisRelita <57762241+VennaErsisRelita@users.noreply.github.com>
2020-01-12 18:22:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,71 @@
+<?php
+session_start();
+require 'fungsi.php';
+
+if (isset($_SESSION['login'])) {
+	$userID = $_SESSION['userID'];	
+}
+
+// query menampilkan semua produk
+$produk = tampil("SELECT * FROM produk");
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>AVE HIJUP</title>
-	<link rel="stylesheet" type="text/css" href="styledm.css">
-	<link rel="stylesheet" type="text/css" href="assets/dist/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
- 	<link href="sb-admin-2.min.css" rel="stylesheet">
-	
-	<script type="text/javascript" src="assets/dist/js/bootstrap.min.js"></script>
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
+	<title>Home AVE HIJUP</title>
+	<link href='img/farhan1.png' rel='shortcut icon'>
+	<link rel="stylesheet" type="text/css" href="vendor/css/style.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css/all.css">
+	<script src="vendor/js/jquery-3.4.1.min.js"></script>
+  	<script src="vendor/js/popper.min.js"></script>
+  	<script src="vendor/js/bootstrap.js"></script>
+  	<script src="vendor/js/all.js"></script>
+  	<script type="text/javascript" src="vendor/js/jquery.js" ></script>
 </head>
 <body>
-	<!-- navbar -->
-<header class="sticky-top">
-	<nav class="navbar  navbar-light bg-light">
+
+<section>
+<header id="navbar">
+	<nav class="navbar navbar-light bg-light">
 		<div class="container-fluid">
+		<?php if (isset($_SESSION['login'])) :?>
+			<div class="nav-item dropdown no-arrow">
+              <a class="" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              	<?php $user = tampil("SELECT * FROM user WHERE userID = $userID") ?>
+              	<?php foreach ($user as $u): ?>
+	                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+	                	<img src="admin/uploaded_files/<?= $u['foto']; ?>" width="20" height="20" class="img-fluid rounded-circle">
+	                  <strong class="mx-2">Wellcome</strong><?= $u['nama']; ?>
+	                </span>	
+              	<?php endforeach ?>
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-left shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="akun.php">
+                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Profile
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#logoutModal">
+                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Logout
+                </a>
+              </div>
+              </div>           
+
+		<?php elseif (!isset($_SESSION['login'])) :?>
 			<div>
-				<a href=""><i class="fas fa-user mr-3"></i>Assalamualaikum Masuk</a>
+				<a href="daftarmasuk.php"><i class="fas fa-user mr-3"></i>Assalamualaikum Masuk</a>
 	  			<span>|</span>
-	  			<a href="">Daftar</a>
+	  			<a href="daftarmasuk.php">Daftar</a>
 	  		</div>
-	  		<div>
-		  		<i class="fas fa-truck mr-3">	Shipping</i>
-		  		<i class="far fa-star mr-3">	Kualitas Pembelian</i>
-				<i class="fab fa-pied-piper-pp mr-3">	Harga Terjangkau</i>
+		<?php endif; ?>
+			<div>
+		  		<i class="fas fa-truck mx-2"></i><span>Shipping</span>
+		  		<i class="fas fa-star mx-2"></i><span>Kualitas Pembelian</span>
+				<i class="fas fa-dollar-sign mx-2"></i><span>Harga Terjangkau</span>
 			</div>
 		</div>
 	</nav>
@@ -39,102 +77,164 @@
 		      <span><i class="fas fa-bars"></i></span>
 		    </button>
 
-  			<a href="login.html" class="navbar-brand">Ave Hijup</a>
+  			<a href="index.php" class="navbar-brand">Ave Hijup</a>
 
   			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     			<span><i class="fas fa-ellipsis-v"></i></span>
   			</button>
 	  		
-	  		<div class="collapse navbar-collapse" id="navbarNav">
+	  		<div class="collapse navbar-collapse collapse" id="navbarNav">
     			<ul class="navbar-nav ml-auto mr-auto">
-      				<li class="nav-item ml-5">
-        				<a href="#">KATEGORI</a>
-      				</li>
-      				<li class="nav-item ml-5">
-        				<a href="#">BEST SELLER</a>
+					<li class="nav-item dropdown ml-3">
+				        <a class="nav-link warna dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				          KATEGORI
+				        </a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					        <?php // query menampilkan kategori
+							$kategori = tampil("SELECT * FROM kategori"); ?>
+					       	<?php foreach ($kategori as $data) { ?>
+					          <a class="dropdown-item mt-2" href="produk.php?kategori=<?= $data['kategoriID']; ?>"><?= $data['namakategori']; ?></a>
+					        <?php } ?>
+					        </div>
+				    </li>
+      				<li class="nav-item ml-3">
+        				<a class="nav-link warna" href="bestseller.php">BEST SELLER</a>
       				</li>
     			</ul>
   				<form class="form-inline my-2 my-lg-0 mr-5">
-		      		<div>
-		      			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			      		<button class="btn btn-secondary my-2 my-sm-0 rounded-pill" type="submit"><i class="fas fa-search"></i></button>
+		      		<div class="wrapper-input">
+		      			<input type="search" placeholder="Search" aria-label="Search" name="cari">
+			      		<button class="my-sm-0" type="submit"><i class="fas fa-search"></i></button>
 			      	</div>
     			</form>
-    			<span><a href="">Hubungi Kami</a><i class="far fa-comment-dots ml-2 mr-4"></i></span>
-    			<span><a href="">Tas Belanja</a><i class="fas fa-shopping-cart ml-2"></i></span>
-  			</div>
-	  	
+    			<span><a href="https://api.whatsapp.com/send?phone=6283847337988&text=%Saya%berminat%dengan%produk%AveHijup&source=&data=">Hubungi Kami</a><i class="far fa-comment-dots ml-2 mr-4"></i></span>
+    			<span>
+    				<a href="keranjang.php">Tas Belanja<i class="fas fa-shopping-cart ml-2"></i> 
+				<?php if (isset($_SESSION['login'])): ?>
+				<?php 
+    				$result = mysqli_query($koneksi,"SELECT * FROM keranjang WHERE userID = $userID");
+    				$cek = mysqli_num_rows($result);
+    			 ?>
+    					<?php if (!$cek == 0): ?>
+    					<span style="position: absolute; margin-left: -10px; margin-top: -7px;" class="badge badge-danger"><?= $cek; ?></span>
+    					<?php endif ?>
+    			<?php endif ?>
+	    				</a>
+	    			</span>
+  			</div>	  	
 	  	</div>
 	</nav>
+
 	<div class="collapse" id="navbarToggleExternalContent">
 	   	<div class="bg-dark p-4">
 	   		<div class="container">
 	   			<ul class="navbar-nav ml-auto mr-auto">
       				<li class="nav-item">
-        				<a href="#">Testimoni</a>
+        				<a href="testimoni.php">Testimoni</a>
+      				</li>
+
+      				<li class="nav-item">
+        				<a href="notifikasi.php">Notifikasi</a>
       				</li>
       				<li class="nav-item">
-        				<a href="#">Notifikasi</a>
+        				<a href="konfirmasi.php">Konfirmasi Pembayaran</a>
       				</li>
       				<li class="nav-item">
-        				<a href="#">Konfirmasi Pembayaran</a>
+        				<a href="akun.php">Akun Saya</a>
       				</li>
       				<li class="nav-item">
-        				<a href="#">Akun Saya</a>
-      				</li>
-      				<li class="nav-item">
-        				<a href="#">Tentang Kami</a>
+        				<a href="infotentangkami.php">Tentang Kami</a>
       				</li>
     			</ul>
     		</div>
 	   	</div>
 	</div>
 </header>
+</section>
 	<!-- navbar akhir -->
+
 	<!-- Konten -->
+<div class="content">
+	<div class="atas container-fluid">
 
-<div class="container">
-	<br>
-	<div id="demo" class="carousel slide" data-ride="carousel">
-
-	  <!-- Indicators -->
-	  <ul class="carousel-indicators">
-	    <li data-target="#demo" data-slide-to="0" class="active"></li>
-	    <li data-target="#demo" data-slide-to="1"></li>
-	    <li data-target="#demo" data-slide-to="2"></li>
-	  </ul>
-
-	  <!-- The slideshow -->
-	  <div class="carousel-inner">
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner rounded">
 	    <div class="carousel-item active">
-	      <img src="gambar1.jpg" alt="Los Angeles" height="800">
+	      <img class="d-block w-100" height="520" src="img/gambar5.jpeg" alt="First slide">
 	    </div>
 	    <div class="carousel-item">
-	      <img src="gambar4.jpg" alt="Chicago"  height="800">
+	      <img class="d-block w-100" height="520" src="img/gambar1.jpg" alt="Second slide">
 	    </div>
 	    <div class="carousel-item">
-	      <img src="gambar3.jpg" alt="New York" height="800">
+	      <img class="d-block w-100" height="520" src="img/gambar4.jpg" alt="Third slide">
 	    </div>
 	  </div>
-
-	  <!-- Left and right controls -->
-	  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-	    <span class="carousel-control-prev-icon"></span>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
 	  </a>
-	  <a class="carousel-control-next" href="#demo" data-slide="next">
-	    <span class="carousel-control-next-icon"></span>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
 	  </a>
-
 	</div>
-	<br>
 </div>
-	<!-- Konten akhir -->
+<br>
+
+<h1 class="text-center mt-5">Produk Terbaru</h1>
+<hr class="w-50">
 
 
+<div class="bawah container-fluid">
 
-	<!-- footer -->
+	<?php 
+	$numcol = 3;
+	$countrow = 0;
+	$colwidth = 12 / $numcol;
+	?>
+	<div class="row m-5">
+	<?php foreach ($produk as $data) { ?>
+		
+		<div class="col-md-<?= $colwidth; ?> mt-5">
+			<a href="detailproduk.php?Produk=<?= $data['produkID'] ?>">
+				<img class="float-left rounded mr-4" height="150" width="150" alt="Card image cap" src="admin/uploaded_files/<?= $data['GambarProduk'] ?>">
+			</a>
+				<div class="mt-1">
+			    	<h5><?= $data['namaproduk'] ?></h5>
+			    	<p>Rp. <?= number_format($data['harga'],2,',','.') ?></p>
+	
+					<?php if ($data['jumlah'] <= 0): ?>
+						<div class="habis">
+				      	<p>Sold Out</p>
+				      	<p class="small"><?= $data['jumlah'] ?> pcs</p>
+						</div>
+					<?php else: ?>
+				      	<div class="ada">
+				      	<p>Ready Stock</p>
+				      	<p class="small"><?= $data['jumlah'] ?> pcs</p>
+						</div>
+					<?php endif ?>
 
-<footer>
+		     	</div>
+		</div>	
+	<?php 
+	$countrow++;
+	if ($countrow % $numcol == 0)
+		echo '</div><div class="row m-5">';
+	}
+	 ?>
+	</div>
+</div>
+</div>
+<!-- Konten akhir -->
+
+<!-- footer -->
+<footer class="mt-5">
 	<div class="container">
 		<div class="row text-center align-items-center">
 			<div class="col-md-4 mt-2 mb-2">
@@ -145,23 +245,41 @@
 			<div class="col-md-4 mt-3">
 				<h5>INFORMASI</h5>
 				<hr class="w-50 mt-2">
-				<a class="btn col-12" href="">Syarat dan Ketentuan</a>
-				<a class="btn col-12" href="">Info pengiriman</a>
-				<a class="btn col-12" href="">Cara belanja</a>
+				<a class="col-12" href="infosyaratdanketentuan.php">Syarat dan Ketentuan</a><br>
+				<a class="col-12" href="infopengiriman.php">Info pengiriman</a><br>
+				<a class="col-12" href="infocarabelanja.php">Cara belanja</a>
 			</div>
 			<div class="col-md-4 mt-2 mb-2">
-				<a href="" class="mr-3">
+				<a href="https://api.instagram.com/v1/self/media/recent?access_token=ACCESS_TOKEN" class="mr-3">
 					<img src="img/ig1.png" width="30" height="30">
 				Ave.Hijup</a>
-				<a href="">
+				<a href="https://api.whatsapp.com/send?phone=6283847337988&text=&source=&data=">
 					<img src="img/wa.png" width="30" height="32">
 				0838-4733-7988</a>
 			</div>
 		</div>
 	</div>
 </footer>
+<!-- footer akhir -->
 
-	<!-- footer akhir -->
+<!-- modal -->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Apa anda ingin keluar?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Klik tombol keluar untuk berganti akun.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Tidak</button>
+          <a class="btn btn-primary" href="logout.php">Keluar</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </body>
 </html>
